8bf4fb35985f1fcb11e7a71b36a687914241aba8
Christopher Garcia <garciawebdev4@gmail.com>
2022-07-13 13:46:59 -0400

    got stuck

--- script.js
+++ script.js
@@ -1,46 +1,58 @@
-let playerSelection = 'Rock'.toLowerCase();
+let playerSelection = prompt('Pick: rock, paper, or scissors').toLowerCase();
 let computerSelection = computerPlay();
+let playerScore = 0;
+let computerScore = 0; 
+
+
+
 
 function computerPlay(){
     let randomNumber = Math.floor(Math.random()*3 + 1);
         switch (randomNumber){
             case 1:
                 return 'rock';
-                break;
             case 2: 
                 return 'paper';
-                break;
             case 3: 
                 return 'scissors';
-                break;
         }
 }
 
 function playRound(playerSelection, computerSelection){
     if(playerSelection == 'rock' && computerSelection == 'rock'){
-        return `It's a tie you both played ${playerSelection}!`;
+        return `It's a tie you both played ${playerSelection}!`
     } else if(playerSelection == 'rock' && computerSelection == 'paper'){
+        computerScore++
         return `You lose! ${computerSelection.charAt(0).toUpperCase() + computerSelection.slice(1)} beats ${playerSelection}!`
     } else if(playerSelection == 'rock' && computerSelection == 'scissors'){
-        return `You win! ${playerSelection.charAt(0).toUpperCase() + playerSelection.slice(1)} beats ${computerSelection}!`
+        playerScore++;
+        return `You win! ${playerSelection.charAt(0).toUpperCase() + playerSelection.slice(1)} beats ${computerSelection}!`    
     }
 
     if(playerSelection == 'paper' && computerSelection == 'paper'){
         return `It's a tie you both played ${playerSelection}!`;
     } else if(playerSelection == 'paper' && computerSelection == 'scissors'){
+        computerScore++
         return `You lose! ${computerSelection.charAt(0).toUpperCase() + computerSelection.slice(1)} beats ${playerSelection}!`
     } else if(playerSelection == 'paper' && computerSelection == 'roc'){
+        playerScore++;
         return `You win! ${playerSelection.charAt(0).toUpperCase() + playerSelection.slice(1)} beats ${computerSelection}!`
     }
 
     if(playerSelection == 'scissors' && computerSelection == 'scissors'){
         return `It's a tie you both played ${playerSelection}!`;
     } else if(playerSelection == 'scissors' && computerSelection == 'rock'){
+        computerScore++
         return `You lose! ${computerSelection.charAt(0).toUpperCase() + computerSelection.slice(1)} beats ${playerSelection}!`
     } else if(playerSelection == 'scissors' && computerSelection == 'paper'){
-        return `You win! ${playerSelection.charAt(0).toUpperCase() + playerSelection.slice(1)} beats ${computerSelection}!`
+        playerScore++;
+        return `You win! ${playerSelection.charAt(0).toUpperCase() + playerSelection.slice(1)} beats ${computerSelection}!` 
     }
 }
 
-console.log(playRound(playerSelection,computerSelection));
-console.log(computerSelection)
+
+
+
+console.log(playerScore);
+console.log(computerScore);
+
